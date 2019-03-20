7b079c4f4ded4fba28210b70b01101e4b711b8f6
MikeN64 <nguyen.michael.van@gmail.com>
2019-03-20 19:58:27 -0400

    Added numbers and suffixes for translating leet

--- json_file_control.py
+++ json_file_control.py
@@ -1,6 +1,6 @@
 import json
 
-with open('cheatsheet/letters.json') as json_file:
+with open('cheatsheet/numbers.json') as json_file:
     letters = json.load(json_file)
     for letter in letters:
         for l in letters[letter]:
